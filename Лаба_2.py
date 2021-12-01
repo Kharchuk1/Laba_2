83ce045c12fd8ab3d0848f35b6139283b8af3d41
Kharchuk1 <arinaka1467@gmail.com>
2021-12-01 22:41:42 +0300

    laba_2

---
+++ Лаба_2.py
@@ -0,0 +1,18 @@
+import PIL as Image
+import PIL as ImageDraw
+from PIL import Image, ImageDraw
+
+with open('DS4.txt') as f:
+    lines = f.readlines()
+
+coor = []
+for line in lines:
+    formatted_line = line[:-1].split(' ')
+    single_coordinate = (int(formatted_line[0]), int(formatted_line[1]))
+    coor.append(single_coordinate)
+
+img = Image.new("RGB", (540, 960), (255, 255, 255))
+draw = ImageDraw.Draw(img)
+draw.point(coor, fill='red')
+img.show()
+img.save("result.jpg")
